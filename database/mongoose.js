fa5a1c35fa86fd6f0caa4066e1d9987daa59b00a
MUHSIRKHAN <muhsirkhan123@gmail.com>
2023-04-15 20:02:59 +0530

    finsished

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB= async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://muhsir:QBBbzCVX3RMFHOE3@cluster0.usyhtns.mongodb.net/?retryWrites=true&w=majority",{
+        await mongoose.connect(process.env.MONGO_URI,{
             useUnifiedTopology: true ,
             useNewUrlParser: true
         })
